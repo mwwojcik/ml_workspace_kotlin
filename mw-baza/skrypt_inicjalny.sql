07653df96ffcbb2f97b275d3b438c3c60f49cd09
mariusz.wojcik <mariusz.wojcik@asseco.pl>
2019-03-25 11:18:47 +0100

    dbinit

---
+++ mw-baza/skrypt_inicjalny.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE reguly (
+   id INT  NOT NULL,
+   nazwa VARCHAR (45),
+   PRIMARY KEY (ID)
+)
+
+create TABLE parametry_reguly(
+id INT  NOT NULL,
+regula_id int not null,
+nazwa VARCHAR (45),
+typ VARCHAR (45),
+wartosc_domyslna VARCHAR (150),
+PRIMARY KEY (ID)
+)
+
+ALTER TABLE parametry_reguly
+  ADD FOREIGN KEY (regula_id) REFERENCES reguly(id)
