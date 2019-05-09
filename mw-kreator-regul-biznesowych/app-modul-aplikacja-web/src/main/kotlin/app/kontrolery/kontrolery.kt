f0ab15829d9ba58320b490641f39a810c184b61f
mariusz.wojcik <mariusz.wojcik@asseco.pl>
2019-05-09 15:51:30 +0200

    Zmiana trybu pracy na embedded, uporzadkowanie skryptow tworzacych schemat

--- mw-kreator-regul-biznesowych/app-modul-aplikacja-web/src/main/kotlin/app/kontrolery/kontrolery.kt
+++ mw-kreator-regul-biznesowych/app-modul-aplikacja-web/src/main/kotlin/app/kontrolery/kontrolery.kt
@@ -8,23 +8,11 @@ import org.springframework.web.bind.annotation.RestController
 import uslugi.RegulyUslugaBean
 import java.util.concurrent.atomic.AtomicLong
 
-data class Greeting(val id: Long, val content: String)
-
 @RestController
 class GreetingController {
 
     @Autowired
     lateinit var regulyUsluga: RegulyUslugaBean
-
-    @Autowired
-    lateinit var generatorKodu: GeneratorKoduBean
-
-
-    val counter = AtomicLong()
-
-    @GetMapping("/greeting")
-    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
-            Greeting(counter.incrementAndGet(), "Hello, $name")
 
     @GetMapping("/reguly")
     fun podajReguly()=regulyUsluga.podajReguly()
