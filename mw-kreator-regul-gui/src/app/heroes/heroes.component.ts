241508680e8cfcd9fd57d9592ac5ddb378c7852d
mwwojcik <mw.wojcik@gmail.com>
2019-05-11 10:36:15 +0200

    tutorial - ukonczony krok Display a Heroes List

--- mw-kreator-regul-gui/src/app/heroes/heroes.component.ts
+++ mw-kreator-regul-gui/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +9,17 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+  selectedHero:Hero
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+  }
 
   constructor() { }
 
