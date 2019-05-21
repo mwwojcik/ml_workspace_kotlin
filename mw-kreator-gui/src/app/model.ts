f92828b2f1775dd635f0d3df59a8c3b93924c872
mariusz.wojcik <mariusz.wojcik@asseco.pl>
2019-05-21 16:34:10 +0200

    model

--- mw-kreator-gui/src/app/model.ts
+++ mw-kreator-gui/src/app/model.ts
@@ -1,5 +1,38 @@
 
 
+/**
+ * 
+ 
+ 
+open  class ObiektBazowy(){
+    var id:Long?=null
+    var wersja:Long?=null
+}
+
+data class Parametr(
+        val nazwa: String
+        , var typ: String? = null
+        , val wartoscDomyslna: String? = ""
+        , val czyUsuwalny:Int=0
+) : ObiektBazowy(),IWalidowalny{}
+
+class ParametrWywolaniaReguly( var nazwaParametruRegulyWolajacej:String=""
+                              ,var nazwaParametruRegulyWolanej  :String )
+
+                              {}
+
+   data class Regula(
+        val kod: String
+        , var tresc: String
+        , val sekwencja: Sekwencja
+        , var parametry: MutableList<Parametr> = mutableListOf()
+        , var wywolaniaRegul: MutableList<WywolanieReguly> = mutableListOf()
+    )                           
+class WywolanieReguly(val kodRegulyWolajacej: String
+                      , val kodRegulyWolanej: String
+                      , var parametry: MutableList<ParametrWywolaniaReguly> = mutableListOf()
+)
+ */
 export class RegulaBiznesowa {
     kod: string;
 }
