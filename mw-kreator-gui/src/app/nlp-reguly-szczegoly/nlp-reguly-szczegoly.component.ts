57b168a741da5f7119ff10837a690c66ee1bb03c
mwwojcik <mw.wojcik@gmail.com>
2019-05-22 22:44:50 +0200

    sparametryzowany routing

--- mw-kreator-gui/src/app/nlp-reguly-szczegoly/nlp-reguly-szczegoly.component.ts
+++ mw-kreator-gui/src/app/nlp-reguly-szczegoly/nlp-reguly-szczegoly.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-nlp-reguly-szczegoly',
   templateUrl: './nlp-reguly-szczegoly.component.html',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NlpRegulySzczegolyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
+  private sub: any;
+  private kod: number;
 
   ngOnInit() {
-  }
+     this.sub = this.route.params.subscribe(params => {
 
+            this.kod= +params['kod'];
+        });
+  }
+  getKod(): number {
+    return this.kod;
+  }
 }
